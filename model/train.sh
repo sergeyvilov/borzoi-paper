ff0f8cfc677fc1b6ce6891aeb3860f3549467d25
Sergey Vilov <sergey.vilov@helmholtz-muenchen.de>
2025-06-16 19:12:06 +0200

    train.sh

--- model/train.sh
+++ model/train.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-westminster_train_folds.py -e borzoi_py310 --f_list 3 -c 4 --identical_crosses -q rtx4090 -o saved_models params.json data/hg38 data/mm10
+project_dir='/lustre/groups/epigenereg01/workspace/projects/vale/gxe-eqtl/borzoi'
+hg38_dir=$project_dir'/training_data/data/hg38'
+mm10_dir=$project_dir'/training_data/data/mm10'
+saved_models=$project_dir'/saved_models/default'
+#saved_models=$project_dir'/saved_models/masked'
+params=/home/icb/sergey.vilov/workspace/gxe-eqtl/borzoi/borzoi/examples/params.json
+
+#westminster_train_folds.py --restart --restore $saved_models  -e borzoi --f_list 3 -c 1 --identical_crosses -q gpu_long -o $saved_models $params $hg38_dir $mm10_dir
+westminster_train_folds.py  -e borzoi --f_list 3 -c 1 --identical_crosses -q gpu_long -o $saved_models $params  $mm10_dir $hg38_dir
